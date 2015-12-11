b9f33205801c0498e8d9051cacaff8b0c8c976fb
tmn <mail@trimn.net>
2015-12-11 21:49:18 +0100

    Add extra packages and descriptions to brew.sh

--- brew.sh
+++ brew.sh
@@ -3,10 +3,12 @@ echo Install all AppStore and XCode at first!
 read -p "Press any key to continue..." -n1 -s
 echo '\n'
 
-
+echo Updates brew
 brew update
 brew upgrade --all
 
+
+echo Install essentials
 brew install wget
 brew install zsh
 brew install git
@@ -16,14 +18,19 @@ brew install tmux
 brew install mosh
 
 
-
 echo tap caskroom/task
 brew tap caskroom/cask
 
+echo Install brew cask stuff
+brew cask install atom
+brew cask install skype
+brew cask install omnifocus
+brew cask install slack
+brew cask install github
+brew cask install iterm2
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install iterm2
 brew cask install dropbox
-
 
 brew cleanup
