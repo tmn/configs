dc9eb90d3bde079ea628bead1cca8ed17a7f1c97
Tri Nguyen <mail@trimn.net>
2016-12-07 23:01:07 +0100

    Remove brew casks

--- brew.sh
+++ brew.sh
@@ -5,10 +5,10 @@ echo '\n'
 
 echo Updates brew
 brew update
-brew upgrade --all
-
+brew upgrade
 
 echo Install essentials
+
 brew install wget
 brew install zsh
 brew install git
@@ -16,21 +16,5 @@ brew install emacs
 brew install node
 brew install tmux
 brew install mosh
-
-
-echo tap caskroom/task
-brew tap caskroom/cask
-
-echo Install brew cask stuff
-brew cask install atom
-brew cask install skype
-brew cask install omnifocus
-brew cask install slack
-brew cask install github
-brew cask install iterm2
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install iterm2
-brew cask install dropbox
 
 brew cleanup
