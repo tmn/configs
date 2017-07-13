1e841cce4941fc38f8571599bd6a198ba5e75b21
Tri Nguyen <mail@trimn.net>
2017-07-13 21:03:29 +0200

    Add new aliases

--- .config/fish/functions/aliases.fish
+++ .config/fish/functions/aliases.fish
@@ -1,5 +1,16 @@
 # System
-alias pbcopy='xsel --clipboard --input'
+alias serve "python -m SimpleHTTPServer"
+alias e "emacs"
+
+## OS Spesific settings
+switch (uname)
+case Linux
+     alias pbcopy 'xsel --clipboard --input'
+end
+
 
 # Git
-alias ss='git status'
+alias ss "git status"
+alias rr "git rebase"
+alias gd "git diff"
+alias gp "git pull"
