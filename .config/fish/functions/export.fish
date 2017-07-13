1c4f0018b5f3b05c2003c4219c85c376d83a4943
Tri Nguyen <mail@trimn.net>
2017-07-13 21:04:25 +0200

    Add new exports

--- .config/fish/functions/export.fish
+++ .config/fish/functions/export.fish
@@ -1,3 +1,12 @@
 ## set env vars
 set -xg PATH $PATH ~/.npm-global/bin
-set -xg TERM "xterm-256color"
+
+## OS Spesific settings
+switch (uname)
+case Linux
+     set -xg TERM "xterm-256color"
+end
+
+set -Ux EDITOR emacs
+set -Ux VISUAL emacs
+set -Ux NVM_DIR "~/.nvm"
